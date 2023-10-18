f21c4d58e673c2d1a5805f7b233441dcf87e46c6
Lucas Cavalcante <lucascpcavalcante@gmail.com>
2023-10-18 18:08:09 +0300

    Remove packer_compiled.lua

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -13,7 +13,7 @@ vim.opt.termguicolors = true
 require("nvim-tree").setup({
   sort_by = "case_sensitive",
   view = {
-    width = 30,
+    width = 50,
   },
   renderer = {
     group_empty = true,
--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -41,5 +41,6 @@ return require('packer').startup(function(use)
     use {
         "nvim-tree/nvim-tree.lua",
     }
+    use('jose-elias-alvarez/null-ls.nvim')
 
 end)
